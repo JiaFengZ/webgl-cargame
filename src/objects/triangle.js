13f93327ed5131fc65bbdb0eaae9036bf95f1109
unknown <1040185058@qq.com>
2019-09-04 22:21:35 +0800

    init 基本结构

---
+++ src/objects/triangle.js
@@ -0,0 +1,13 @@
+export default class Triangle {
+  constructor(params) {
+    this.name = "Triangle"
+    this.vertices = new Float32Array([
+      0, 0, 0,
+      0.5, 0, -1,
+      -0.5, 0, -1
+    ])
+    this.triangleIndices = new Uint16Array([0, 1, 2])
+    this.numVertices  = 3
+    this.numTriangles = 1
+  }
+}
