ca214d6fb90004ba664ce7122497fcdfb8a92969
unknown <1040185058@qq.com>
2019-11-05 07:54:54 +0800

    fix 修复汽车和树木渲染问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,31 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    contentBase: './dist'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      }
+module.exports = (env) => {
+  return {
+    mode: env,
+    entry: './src/index.js',
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+      contentBase: './dist'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [ 'style-loader', 'css-loader' ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ // 打包输出HTML
+        title: 'webgl-cargame',
+        filename: 'index.html',
+        template: './src/index.html'
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ // 打包输出HTML
-      title: 'webgl-cargame',
-      filename: 'index.html',
-      template: './src/index.html'
-    })
-  ]
+  }
 }
